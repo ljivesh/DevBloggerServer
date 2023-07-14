78866fca6703616b19ac112a9e0d79a2154dfdcc
Menma <ljivesh@gmail.com>
2023-07-14 18:55:16 +0530

    CORS Update

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const db = require('./db').default;
 const configs = require('./configs');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const userRoute = require('./routes/userRoutes');
